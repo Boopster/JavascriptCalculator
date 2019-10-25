30ab209f873b0c841d7b220f7a13e7a8bd3df222
Rebecca Still <beckystill@hotmail.com>
2019-10-25 20:21:06 +0100

    it can add 1 to 4 and get 5

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -1,14 +1,21 @@
-var Calculator = require('../../public/js/calculator.js')
-var assert = require('assert')
+const Calculator = require('../../public/js/calculator.js');
+const assert = require('assert');
 
 describe('calculator', function () {
+  // let calculator;
   beforeEach(function () {
-    calculator = new Calculator()
+    calculator = new Calculator();
   });
 
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
-    assert.equal(true, true)
-  })
+    assert.equal(true, true);
+  });
+
+  it('it can add 1 to 4 and get 5', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.equal(calculator.runningTotal,5);
+  });
 
 });
