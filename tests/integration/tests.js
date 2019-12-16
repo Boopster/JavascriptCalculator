96ecd55819190400d8e318d533a1d2c9cb8cef07
Rebecca Still <beckystill@hotmail.com>
2019-12-16 16:10:15 +0000

    add answers to UI Integration Tests

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -46,15 +46,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-6');
   });
 
-  // it('should display the expected output for a range of numbers__decimal numbers', function(){
-  //   element(by.css('#number2')).click();
-  //   element(by.css('#number_decimal')).click();
-  //   element(by.css('#number8')).click();
-  //   element(by.css('#operator_subtract')).click();
-  //   element(by.css('#number1')).click();
-  //   element(by.css('#operator_equals')).click();
-  //   expect(running_total.getAttribute('value')).to.eventually.equal('1.8');
-  // });
+  it('should display the expected output for a range of numbers__decimal numbers', function(){
+    element(by.css('#number2')).click();
+    element(by.css('#number_decimal')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.4');
+  });
   //
   // it('should display the expected output for a range of numbers__large numbers', function(){
   // });
@@ -63,8 +63,8 @@ describe('calculator functionality', function() {
   // });
 
 });
-// 1. Do the number buttons update the display of the running total?
-// 2. Do the arithmetical operations update the display with the result of the operation?
-// 3. Can multiple operations be chained together?
+// 1. Do the number buttons update the display of the running total? - yes
+// 2. Do the arithmetical operations update the display with the result of the operation? - yes
+// 3. Can multiple operations be chained together? - yes
 // 4. Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
 // 5. What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
